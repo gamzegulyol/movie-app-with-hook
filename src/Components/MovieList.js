a885199c84c6084d7b96cb287783b18d03c043d6
gamzegulyol <ggulyol@gmail.com>
2019-12-14 15:48:11 +0300

    MovieList component has created

---
+++ src/Components/MovieList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovieList = () => {
+    return(
+        <div>
+            <h5>Movie Title</h5>
+            <p>Movie Description</p>
+        </div>
+
+    );
+};
+
+export default MovieList;
